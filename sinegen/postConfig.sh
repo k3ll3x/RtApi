9a08f075acf8c03e1ecbc8ffc1f64998ce1b8bf0
k3ll3x <k3ll3x@gmail.com>
2023-06-02 15:41:48 +0200

    sinegenerator sending frames though udp multicast

--- sinegen/postConfig.sh
+++ sinegen/postConfig.sh
@@ -2,5 +2,5 @@
 # {"username":"xyz","password":"xyz"}
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data "$(cat $1 | tr -d \n | tr -d ' ')" \
+  --data "$(cat $1 | tr -d '\n' | tr -d ' ')" \
   http://localhost:8000/config
--- sinegen/src/main.rs
+++ sinegen/src/main.rs
@@ -4,17 +4,16 @@ use std::{thread, time};
 use serde::{Serialize, Deserialize};
 use clap::Parser;
 use rand;
-use std::collections::HashMap;
+// use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use std::fs;
 use rocket::{serde::json::Json, State};
+use toml;
+use dashmap::DashMap;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    // #[arg(short, long, required = true)]
-    // protocol: String,
-    #[arg(short, long, required = false)]
-    bind_addr: String,
     #[arg(short, long, required = false)]
     multicast_addr: String,
     #[arg(short, long)]
@@ -40,18 +39,21 @@ struct Line{
     channels: Vec<WaveConf>
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 struct Config{
     lines: Vec<Line>
 }
 
-type TWaveMap = HashMap<u16, Vec<WaveConf>>;
+// type TWaveMap = HashMap<u16, Vec<WaveConf>>;
+type TWaveMap = DashMap<u16, Vec<WaveConf>>;
 type TSession = Arc<Mutex<Session>>;
 
 #[derive(Debug)]
 struct Session{
     wavemap: TWaveMap,
-    socket: UdpSocket,
+    // socket: UdpSocket,
+    // sockets: HashMap<u16, UdpSocket>,
+    sockets: DashMap<u16, UdpSocket>,
     multicast_addr: String
 }
 
@@ -63,7 +65,24 @@ async fn index() -> Json<Frame> {
     Json(frame)
 }
 
-fn load_config(_session: &State<TSession>, cfg: Config) {
+#[get("/config")]
+async fn getwavemap(_session: &State<TSession>) -> String {
+    let session = _session.clone().lock().unwrap();
+    let wavemap = &session.wavemap;
+    format!("{:?}", wavemap)
+}
+
+#[post("/config", format = "json", data = "<incfg>")]
+async fn api_config(session: &State<TSession>, incfg: Json<Config>) -> Json<Frame> {
+    // load_config(session, incfg.into_inner());
+    // create_threads(session.lock());
+    Json(Frame { points: randf32_vec(3) })
+}
+
+fn load_config(_session: Arc<Mutex<Session>>, cfg: Config) {
+    let session_clone = _session.clone();
+    let mut session = session_clone.lock().unwrap();
+    session.wavemap.clear();
     for line in &cfg.lines{
         let mut channels = Vec::new();
         for channel in &line.channels{
@@ -78,68 +97,95 @@ fn load_config(_session: &State<TSession>, cfg: Config) {
         }
         
         let appid = line.appid.clone();
-        
-        let session_clone = _session.clone();
-
-        let mut session = session_clone.lock().unwrap();
         session.wavemap.insert(appid, channels);
+
+        let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, appid)).unwrap();
         let multicast_addr: SocketAddrV4 = format!("{}:{}", session.multicast_addr, appid).as_str().parse().unwrap();
-        session.socket.join_multicast_v4(&multicast_addr.ip(), &Ipv4Addr::UNSPECIFIED).unwrap();
-        drop(session);
+        socket.join_multicast_v4(&multicast_addr.ip(), &Ipv4Addr::UNSPECIFIED).unwrap();
+        session.sockets.insert(appid, socket);
     }
 }
 
-#[post("/config", format = "json", data = "<incfg>")]
-async fn api_config(session: &State<TSession>, incfg: Json<Config>) -> Json<Frame> {
-    load_config(session, incfg.into_inner());
-    Json(Frame { points: randf32_vec(16) })
+fn get_smv(config: &Vec<WaveConf>) -> Vec<f32>{
+    let mut smv = Vec::<f32>::new();
+    for channel in config{
+        smv.push(channel.lst[0] * (2.0 * 3.14159 * channel.frequency * rand::random::<f32>() + channel.phase_shift + channel.offset).sin());
+    }
+    smv
 }
-
-// fn get_smv(_session: &State<TSession>, appid: u16) -> Vec<f32>{
-//     let session_clone = _session.clone();
-//     let mut session = session_clone.lock().unwrap();
-//     return (
-//         sine_conf[appid][channel_index].amplitude[0] *
-//         std::sin(2 * M_PI * sine_conf[appid][channel_index].frequency *
-//         time_in_secs[appid] + sine_conf[appid][channel_index].phase_shift) + sine_conf[appid][channel_index].offset
-//     );
-// }
 
 fn randf32_vec(n: i32) -> Vec<f32>{
     (1..n).map(|x| x as f32 * rand::random::<f32>()).collect()
+}
+
+fn create_threads(session: Arc<Mutex<Session>>){
+    let session = session.clone();
+    thread::spawn(move || {
+        let session = session.lock().unwrap();
+        // println!("{:?}", session.wavemap.iter().count());
+        for e in session.wavemap.iter() {
+            let appid = e.key().clone();
+            let config = e.value().clone();
+            let socket = session.sockets.get(&appid).unwrap().try_clone();
+            let multicast_addr = session.multicast_addr.to_string();
+            thread::spawn(move || {
+                // let config = config.clone();
+                println!("{:?}", config);
+                loop {
+                    let multicast_addr = multicast_addr.clone();
+                    let frame = Frame {
+                        // points: get_smv(config)
+                        // points: randf32_vec(rand::random::<i32>())
+                        points: randf32_vec(8)
+                    };
+                    let buffer = bincode::serialize(&frame).unwrap();
+                    socket.as_ref().unwrap().send_to(&buffer, (multicast_addr, appid)).unwrap();
+                    thread::sleep(std::time::Duration::from_secs(1));
+                }
+            });
+        }
+        thread::sleep(std::time::Duration::from_secs(1));
+    }).join().unwrap();
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
     println!("{args:?}");
-    
+
     let session = Session {
         wavemap: TWaveMap::new(),
-        socket: UdpSocket::bind(format!("{}", args.bind_addr)).unwrap(),
+        // socket: UdpSocket::bind(format!("{}", args.bind_addr)).unwrap(),
+        // sockets: HashMap::new(),
+        sockets: DashMap::new(),
         multicast_addr: args.multicast_addr
+        // start: bool
     };
     let shsession = Arc::new(Mutex::new(session));
     
-    let session_clone = shsession.clone();
-    thread::spawn( move || {
-        loop {
-            let s = session_clone.lock().unwrap();
-            for i in &s.wavemap {
-                println!("{:?}",i);
-                let frame = Frame {
-                    points: randf32_vec(rand::random::<i32>() % 16)
-                };
-                let buffer = bincode::serialize(&frame).unwrap();
-                // s.socket.send_to(&buffer, &s.multicast_addr).unwrap();
-                s.socket.send_to(&buffer, format!("{}:{}", &s.multicast_addr, "1233")).unwrap();
-                thread::sleep(time::Duration::from_secs(1));
-            }
+    //load toml config
+    let configfile = args.configfile.as_str();
+    let contents = fs::read_to_string(configfile).unwrap();
+    let mut config: Config = Config::default();
+    match configfile {
+        f if f.ends_with(".toml") => {
+            config = toml::from_str(&contents.as_str()).unwrap();
+        },
+        f if f.ends_with(".json") => {
+            config = serde_json::from_str(&contents.as_str()).unwrap();
+        },
+        _ => {
+            println!("Unknown file type: {}", configfile);
         }
-    });
+    }
 
+    // println!("{:?}", config);
+
+    load_config(shsession.clone(), config);
+    create_threads(shsession.clone());
+    
     rocket::build()
         .manage(shsession.clone())
-        .mount("/", routes![index, api_config])
+        .mount("/", routes![index, api_config, getwavemap])
         .launch().await;
 }
